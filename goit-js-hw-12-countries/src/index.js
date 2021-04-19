d534e5857fad0adfa7a24a1b0ff828aeb8d8a88a
LoraZahorzhevska <Zagorzhevskaya@gmail.com>
2021-04-20 01:18:27 +0300

    добавляет разметку одной карточки

--- goit-js-hw-12-countries/src/index.js
+++ goit-js-hw-12-countries/src/index.js
@@ -1,1 +1,20 @@
 import './styles.css';
+import countryCardTpl from './templates/country-card.hbs';
+
+const refs = {
+  cardContainer: document.querySelector('.js-card-container'),
+};
+
+fetch('https://restcountries.eu/rest/v2/name/Ukraine')
+  .then(response => {
+    return response.json();
+  })
+  .then(country => {
+    console.log(country);
+    const markup = countryCardTpl(...country);
+    console.log(markup);
+    refs.cardContainer.innerHTML = markup;
+  })
+  .catch(error => {
+    console.log(error);
+  });
